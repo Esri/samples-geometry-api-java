875f8024da687c4483f04396d9223a4453387ec5
will <willtemperley@gmail.com>
2016-11-15 10:06:53 +0100

    License headers added

--- ShapefileGeometryCursor/src/main/java/com/esri/core/geometry/examples/MixedEndianDataInputStream.java
+++ ShapefileGeometryCursor/src/main/java/com/esri/core/geometry/examples/MixedEndianDataInputStream.java
@@ -1,3 +1,16 @@
+/*
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
 package com.esri.core.geometry.examples;
 
 import java.io.DataInputStream;
@@ -17,7 +30,7 @@ class MixedEndianDataInputStream extends DataInputStream {
     }
 
     /**
-     * copied from {@link DataInputStream#readInt()} just reversing the variable names
+     * modification of {@link DataInputStream#readInt()} reversing byte order
      */
     final int readLittleEndianInt() throws IOException {
         int ch4 = in.read();
@@ -30,7 +43,7 @@ class MixedEndianDataInputStream extends DataInputStream {
     }
 
     /**
-     * copied from {@link DataInputStream#readLong()} ()} just reversing the array indices
+     * modification {@link DataInputStream#readLong()} ()} reversing the array indices
      */
     private long readLittleEndianLong() throws IOException {
         readFully(readBuffer, 0, 8);
